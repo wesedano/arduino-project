7774f5ff0a3b7f52c618424e285d2db33482245b
willy sedano <wesedano@ieee.org>
2025-06-20 01:05:18 -0500

    Update blink sketch

--- main/main.ino
+++ main/main.ino
@@ -5,7 +5,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED on
-  delay(1000);                      // Wait 1 second
+  delay(100);                      // Wait 1 second
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED off
-  delay(1000);                      // Wait 1 second
+  delay(100);                      // Wait 1 second
 }
