bcc2b2620ea238433ce0cfadf39c8cea4b3810b7
Willy E Sedano <71334009+wesedano@users.noreply.github.com>
2025-06-20 19:57:34 -0500

    Extend blink duration to 2 seconds

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+// Blink the built-in LED for 2 seconds on and 2 seconds off.
 void setup() {
   // Initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -5,7 +6,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED on
-  delay(100);                      // Wait 1 second
+  delay(2000);                      // Wait 2 seconds
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED off
-  delay(500);                      // Wait 1 second
+  delay(2000);                      // Wait 2 seconds
 }
